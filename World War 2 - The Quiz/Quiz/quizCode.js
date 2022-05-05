bd2faaa8dfab36d439e75e81b449764670277517
40510238 <40510238@live.napier.ac.uk>
2022-05-05 23:36:02 +0100

    Project Update

---
+++ World War 2 - The Quiz/Quiz/quizCode.js
@@ -0,0 +1,34 @@
+const quiz = document.getElementById('quiz');
+const results = document.getElementById('results');
+const submit = document.getElementById('submit');
+
+function quiz() {}
+
+function results() {}
+
+const questions = [
+    {
+        question: "The events of which year are typically said to mark the beginning of World War 2?",
+        answers: {
+            a: "1938",
+            b: "1939",
+            c: "1940"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which of the following events marked the 'official' beginning of World War 2?",
+        answers: {
+            a: "Nazi Germany's invasion of Poland",
+            b: "Japan's attack on Pearl Harbor",
+            c: "Russia's invasion of Finland"
+        },
+        correctAnswer: "a"
+    },
+    {
+
+    }
+]
+
+finishButton.addEventListener('click', results);
+
